b57b63a64f99db3165e304594f099b6d4024e131
SomethingElseIGuess <swormac@uci.edu>
2025-01-10 11:51:18 -0800

    working

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,55 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 direction = new Vector2(horizontal, vertical);
+
+        direction = direction.normalized;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+        _playerTransform.Translate(Vector2.up * _speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+        _playerTransform.Translate(Vector2.down * _speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+        _playerTransform.Translate(Vector2.left * _speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+        _playerTransform.Translate(Vector2.right * _speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && _numSeedsLeft < 0)
+        {
+            PlantSeed();
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+
+        _numSeedsLeft--;
+
+        _numSeedsPlanted++;
+
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
